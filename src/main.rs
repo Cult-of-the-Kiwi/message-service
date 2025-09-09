94f4869c67c348de9f89950179fef9d5df06f5c7
Marcelo Domínguez <dmmarcelo27@gmail.com>
2025-09-09 19:37:40 +0200

    Initialize repo

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use message_service::{app, config, state::AppState};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let db = config::init_db().await?;
+    let state = AppState::new(db).await?;
+
+    let app = app::build(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
